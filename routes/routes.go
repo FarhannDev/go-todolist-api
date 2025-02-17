1551cb420d2d7de1d957071d2245bba5bfbfb287
Farhan <101630148+KucingOyenBarbar@users.noreply.github.com>
2025-02-17 07:11:26 +0700

    add routes

---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"todolist-api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() {
+	r := gin.Default()
+
+	// Endpoint task
+	r.POST("/task", controllers.AddTask)
+	r.GET("/task", controllers.GetTasks)
+	r.PUT("/task/:id/complete", controllers.CompleteTask)
+	r.DELETE("/task/:id", controllers.DeleteTask)
+}
